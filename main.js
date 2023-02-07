325c0df167568550700a206b97e4afb64be88e6a
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-02-07 21:03:16 +0530

    saving objects in local storege

--- main.js
+++ main.js
@@ -3,20 +3,25 @@ window.onload = function(e) {
 
     // Check for LocalStorage support.
     if (localStorage) {
-  
-      // Add an event listener for form submissions
-      document.getElementById('my-form').addEventListener('submit', function(e) {
-        
-        e.preventDefault();
 
-        var name = document.getElementById('name').value;
-        console.log(name);
-        var email = document.getElementById('email').value;
-  
-        // Saving in localStorage.
-        localStorage.setItem('name', name);
-        localStorage.setItem('email', email);
-      });
-  
+        // Add an event listener for form submissions
+        document.getElementById('my-form').addEventListener('submit', function (e) {
+
+            // e.preventDefault();
+            var name = document.getElementById('name').value;
+            // console.log(name);
+            var email = document.getElementById('email').value;
+            let myobj = {
+                name: `${name}`,
+                email: `${email}`
+            }
+            let myobjnew = JSON.stringify(myobj);
+            localStorage.setItem('myobj', myobjnew)
+            // Saving in localStorage.
+            // localStorage.setItem('name', name);
+            // localStorage.setItem('email', email);
+        });
+
+    }
     }
 }
